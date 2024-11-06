702daf3f4ca53d056d598f8c391c4e39e16570e9
Shahariar <mononshahariar@gmail.com>
2024-11-06 22:47:35 +0600

    version 01

---
+++ app/controllers/blogController.js
@@ -0,0 +1,23 @@
+
+// Create
+
+export const createBlog =  async (req, res) => {
+    return res.json({message:'Blog created successfully'});
+}
+
+// Read
+export const readBlog =  async (req, res) => {
+    return res.json({message:'Blog read successfully'});
+}
+
+
+// Update
+export const updateBlog =  async (req, res) => {
+    return res.json({message:'Blog update successfully'});
+}
+
+
+// Delete
+export const deleteBlog =  async (req, res) => {
+    return res.json({message:'Blog delete successfully'});
+}
---
+++ routes/api.js
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+import * as blogController from "../app/controllers/blogController.js";
+
+
+// Create Route
+router.post("/create-blog",blogController.createBlog);
+
+// Read Route
+router.get("/read-blog",blogController.readBlog);
+
+// Update Route
+router.put("/update-blog",blogController.updateBlog);
+
+// Delete Route
+router.delete("/delete-blog",blogController.deleteBlog);
+
+
+export default router;
